9bc1e36b1df2a56c5db8a947e89f7dabb3c647dd
RahmTahr <tahrima99@gmail.com>
2020-03-14 11:44:17 -0400

    Added imports to other main

--- Midterm_Project/src/Main.cpp
+++ Midterm_Project/src/Main.cpp
@@ -1,5 +1,9 @@
-
+#include <stdio.h> //scanf, fflush, etc.
+#include <ctype.h> //for comparing chars
+#include <string.h> //string processing functions
+#include <cstring>
 #include <iostream>
+
 using namespace std;
 
 int main() {
