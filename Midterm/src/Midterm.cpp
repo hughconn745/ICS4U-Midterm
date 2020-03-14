771a41faeacdcffde3802a63fd041774b5f4a82b
RahmTahr <tahrima99@gmail.com>
2020-03-14 11:41:06 -0400

    Added imports needed to that my functionalities depend on

--- Midterm/src/Midterm.cpp
+++ Midterm/src/Midterm.cpp
@@ -1,4 +1,7 @@
-
+#include <stdio.h> //scanf, fflush, etc.
+#include <ctype.h> //for comparing chars
+#include <string.h> //string processing functions
+#include <cstring>
 #include <iostream>
 
 using namespace std;
