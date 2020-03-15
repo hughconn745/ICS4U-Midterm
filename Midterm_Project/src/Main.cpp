358e7c39f9d9ee46c06b5b0bd8c43aaa9ab83b38
RahmTahr <tahrima99@gmail.com>
2020-03-14 20:46:01 -0400

    added comments to main

--- Midterm_Project/src/Main.cpp
+++ Midterm_Project/src/Main.cpp
@@ -8,7 +8,21 @@ using namespace std;
 
 int main() {
 
-
+	/*THINGS TO CONSIDER WHEN PROGRAMMING MAIN:
+	 * I'm guessing that the program will run through a while loop with the condition looking something like
+	 * while (1){
+	 * }
+	 * or will the user have the option to exit?
+	 *
+	 * the search function will accept an int pointer pointing to the first element of the int array so we need something like:
+	 *
+	 * int indexes[size of user input];
+	 * int *indexPtr = &index[0];
+	 *
+	 *
+	 *
+	 *
+*/
 
 
 
